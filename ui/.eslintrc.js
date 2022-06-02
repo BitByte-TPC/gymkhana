a95b263f479e3fd3f3febff9b12884d740946fba
Aksh Bansal <akshmark@gmail.com>
2022-06-02 12:33:52 +0530

    chore: setup eslint & prettier

---
+++ ui/.eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  plugins: ["@typescript-eslint"],
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: "module",
+  },
+  rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
+    ],
+  },
+};
