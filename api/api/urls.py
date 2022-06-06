5f0198f6b220e8c931b26d30c1112839c932fe03
Varisha Rashid <varisharashid@Varishas-MacBook-Air.local>
2022-06-06 14:52:30 +0530

    feat(api): added admin panel

--- api/api/urls.py
+++ api/api/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from django.urls.conf import include
+from django.contrib import admin
 from .views import ping_handler
-
+import os
 urlpatterns = [
     path('ping/', ping_handler, name='ping'),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
+
+if os.environ.get('ENV')=='dev':
+    urlpatterns.append(path('admin/',admin.site.urls))
