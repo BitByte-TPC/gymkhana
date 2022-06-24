1bea7a3d4bde2bcf207919596426c82a37cdca8d
Aksh Bansal <akshmark@gmail.com>
2022-06-24 23:42:03 +0530

    feat(ui): add news carousel

--- ui/src/pages/Home/index.tsx
+++ ui/src/pages/Home/index.tsx
@@ -1,3 +1,15 @@
+import {NewsCarousel} from '../../components/NewsCarousel';
+import {TopBar} from '../../components/TopBar';
+import styles from './styles.module.css';
+
 export const Home: React.FC = () => {
-  return <div>Homepage</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.dummyNav}>nav</div>
+      <div className={styles.mainContent}>
+        <TopBar />
+        <NewsCarousel />
+      </div>
+    </div>
+  );
 };
