d79561edb9d49e11b532b1942d90eae3bf504589
Aksh Bansal <akshmark@gmail.com>
2022-07-02 18:51:21 +0530

    feat(ui): add profile info in topbar

--- ui/src/components/TopBar/index.tsx
+++ ui/src/components/TopBar/index.tsx
@@ -1,5 +1,15 @@
 import styles from './styles.module.css';
 
 export const TopBar: React.FC = () => {
-  return <div className={styles.container}>profile</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.profileInfoContainer}>
+        <img
+          src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png"
+          alt="profile-img"
+        />
+        <div className={styles.profileName}>Name</div>
+      </div>
+    </div>
+  );
 };
