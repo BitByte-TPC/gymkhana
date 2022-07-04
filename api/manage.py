69a2968c5d0d4d67f3b0ef182a93a3f4b8351cb7
brijsiyag <brijsiyag@gmail.com>
2022-07-04 14:18:48 +0530

    refactor(api): settings module split for dev and prod

--- api/manage.py
+++ api/manage.py
@@ -9,7 +9,7 @@ from dotenv import load_dotenv
 def main():
     """Run administrative tasks."""
     load_dotenv()
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings.dev')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
