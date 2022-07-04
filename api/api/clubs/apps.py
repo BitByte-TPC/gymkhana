cc9989ca31d1b7afbee64946eb7bf878ae142d4c
Pranshu <pranshunayak566@gmail.com>
2022-07-04 16:50:31 +0530

    feat(api): add model for storing clubs information

---
+++ api/api/clubs/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class ClubsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'clubs'
+    label = 'clubs'
---
+++ api/api/clubs/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Club (models.Model):
+    CULTURAL_CLUB = 'Cultural'
+    SCIENCE_AND_TECH_CLUB = 'Science & Technology'
+    SPORTS_CLUB = 'Sports'
+
+    CLUB_TYPE_CHOICES = [
+        ('Cultural', CULTURAL_CLUB),
+        ('S&T', SCIENCE_AND_TECH_CLUB),
+        ('Sports', SPORTS_CLUB)
+    ]
+
+    name = models.CharField(max_length=100, unique=True)
+    category = models.CharField(max_length=10, choices=CLUB_TYPE_CHOICES)
+    description = models.TextField()
+    email = models.EmailField(max_length=50, unique=True)
+    logo = models.URLField(max_length=200, unique=True)
