bd0b200285e35cd38bcf220af89bfb93cb8e9c72
Prajjwal Kapoor <prajjwal.kapoor.mail@gmail@con>
2022-07-12 01:48:15 +0530

    refactor(ui): layout component for pages

--- ui/src/components/Sidebar/SidebarRow.tsx
+++ ui/src/components/Sidebar/SidebarRow.tsx
@@ -10,7 +10,10 @@ interface Props {
 
 export const SidebarRow: React.FC<Props> = props => {
   return (
-    <div className={styles.container_row} onClick={props.onClick}>
+    <div
+      className={styles.container_row}
+      onClick={() => props.onClick && props.onClick()}
+    >
       <div className={styles.row_content}>
         <div className={styles.icondesign}>
           <img src={props.icon} alt="icon" />
--- ui/src/pages/Profile/index.tsx
+++ ui/src/pages/Profile/index.tsx
@@ -1,18 +1,10 @@
+import {Layout} from '../../components/Layout/Layout';
 import {ProfileCard} from '../../components/ProfileCard';
-import {Sidebar} from '../../components/Sidebar/Sidebar';
-import {TopBar} from '../../components/TopBar';
-import styles from './styles.module.scss';
 
 export const Profile: React.FC = () => {
   return (
-    <div className={styles.container}>
-      <div className={styles.NavContainer}>
-        <Sidebar />
-      </div>
-      <div className={styles.mainContent}>
-        <TopBar />
-        <ProfileCard />
-      </div>
-    </div>
+    <Layout>
+      <ProfileCard />
+    </Layout>
   );
 };
