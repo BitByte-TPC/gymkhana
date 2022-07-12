b11e5ec41c5108d848dd9c04aa66f1dec2931d36
Pranshu <pranshunayak566@gmail.com>
2022-07-12 14:27:49 +0530

    feat(api): add endpoint to get all clubs details

--- api/api/clubs/tests.py
+++ api/api/clubs/tests.py
@@ -1,22 +1,35 @@
+from django.contrib.auth import get_user_model
 from rest_framework.test import APITestCase
 
 from api.clubs.models import Club
+
+User = get_user_model()
 
 
 class ListClubsViewTest(APITestCase):
     """Tests ListClubView for getting the list of clubs."""
 
     def testListClubs_noClubExistsInDb_returnsEmptyList(self):
-        url = '/clubs/'
-        response = self.client.get(url, format='json')
+        # given
+        user = User.objects.create(
+            email='test@user.com', first_name='Test', last_name='User')
+        self.client.force_authenticate(user)
+        # when
+        response = self.client.get('/clubs/', format='json')
+        # then
         self.assertEqual(Club.objects.count(), len(response.data))
 
     def testListClubs_oneClubExistsInDb_returnsOneClubInResponse(self):
-        url = '/clubs/'
+        # given
+        user = User.objects.create(
+            email='test@user.com', first_name='Test', last_name='User')
+        self.client.force_authenticate(user)
         Club.objects.create(name="Bitbyte - The Programming Club",
                             category="S&T",
                             description="Aim to create thriving coding environment for developers.",
                             email="theprogclub@iiitdmj.ac.in <theprogclub@iiitdmj.ac.in>;",
                             logo="https://www.iiitdmj.ac.in/webix.iiitdmj.ac.in/tpclogo.png")
-        response = self.client.get(url, format='json')
+        # when
+        response = self.client.get('/clubs/', format='json')
+        # then
         self.assertEqual(Club.objects.count(), len(response.data))
--- api/api/urls.py
+++ api/api/urls.py
@@ -9,8 +9,9 @@ from .views import ping_handler
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('auth/', include('api.auth.urls')),
-    path('ping/', ping_handler, name='ping'),
-    path('clubs/', include('api.clubs.urls'))
+    path('clubs/', include('api.clubs.urls')),
+    path('ping/', ping_handler, name='ping')
+
 ]
 
 if os.environ.get('ENV') == 'dev':
