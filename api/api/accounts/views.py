cfa4b71de93055acb6574edfe1534e12d35c87d4
Armin Patel <72291325+arminpatel@users.noreply.github.com>
2022-07-15 19:18:19 +0530

    fix(api): null issue in api.accounts.models.py

--- api/api/accounts/views.py
+++ api/api/accounts/views.py
@@ -1,5 +1,4 @@
 from rest_framework import generics
-from rest_framework.permissions import AllowAny
 
 from .models import User
 from .serializers import UserSerializer
@@ -7,6 +6,5 @@ from .serializers import UserSerializer
 
 class UsersListView(generics.CreateAPIView):
     """ View to create users """
-    permission_classes = (AllowAny,)
     queryset = User.objects.all()
     serializer_class = UserSerializer
