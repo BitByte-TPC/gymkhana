aaae38e5142a11b4493d26a7af6a6cc7f73bff09
Prajjwal Kapoor <prajjwal.kapoor.mail@gmail@con>
2022-07-15 23:20:25 +0530

    fix(ui): fetch clubs from api

--- ui/src/components/ClubsCarousel/index.tsx
+++ ui/src/components/ClubsCarousel/index.tsx
@@ -1,37 +1,47 @@
 import styles from './styles.module.scss';
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
-import {dummyClubLogo} from '../../assets';
-
-const data = [
-  {
-    name: 'TPC',
-    category: 'Technical',
-    logoUrl: dummyClubLogo,
-  },
-  {
-    name: 'Jazbaat',
-    category: 'Cultural',
-    logoUrl: dummyClubLogo,
-  },
-  {
-    name: 'Basketball',
-    category: 'Sports',
-    logoUrl: dummyClubLogo,
-  },
-  {
-    name: 'Saaz',
-    category: 'Cultural',
-    logoUrl: dummyClubLogo,
-  },
-  {
-    name: 'Badminton',
-    category: 'Sports',
-    logoUrl: dummyClubLogo,
-  },
-];
+import {useEffect, useState} from 'react';
+import {useNavigate} from 'react-router-dom';
+import {SERVER_URL} from '../../globals/constants';
 
 export const ClubsCarousel: React.FC = () => {
+  interface ClubsCarouselData {
+    name: string;
+    logo: string;
+    category: string;
+  }
+  const [data, setData] = useState<ClubsCarouselData[]>([]);
+  const token = window.sessionStorage.getItem('token');
+  const CLUBS_API_URL = SERVER_URL + '/clubs';
+  const navigate = useNavigate();
+  const invalidTokenHandler = () => {
+    const errQuery = new URLSearchParams({
+      error: 'Please try again later!',
+    });
+
+    window.sessionStorage.removeItem('token');
+    navigate('/login?' + errQuery.toString());
+  };
+  const getClubs = async () => {
+    const response = await fetch(CLUBS_API_URL, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    if (response) {
+      if (response.status === 200) {
+        const data = await response.json();
+        setData(data);
+      } else if (response.status === 403) {
+        token && invalidTokenHandler();
+      }
+    }
+  };
+  useEffect(() => {
+    getClubs();
+  }, []);
+
   const responsive = {
     desktop: {
       breakpoint: {max: 5000, min: 1024},
@@ -50,9 +60,9 @@ export const ClubsCarousel: React.FC = () => {
     <div className={styles.container}>
       <div className={styles.heading}>Clubs</div>
       <Carousel responsive={responsive} className={styles.carousel}>
-        {data.map((club, i) => (
-          <div key={i} className={styles.card}>
-            <img src={club.logoUrl} alt="club" />
+        {data.map((club, index) => (
+          <div key={index} className={styles.card}>
+            <img src={club.logo} alt="club" />
             <div>
               <div className={styles.name}>{club.name}</div>
               <div className={styles.category}>{club.category}</div>
