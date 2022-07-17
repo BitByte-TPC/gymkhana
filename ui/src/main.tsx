79951d95000a1682b30c7c429a6f0fd02ee4b794
Aksh Bansal <akshmark@gmail.com>
2022-07-18 00:40:26 +0530

    feat(ui): add swr

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import {App} from './App';
 import {BrowserRouter} from 'react-router-dom';
 import './index.scss';
+import {SWRConfig} from 'swr';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <SWRConfig>
+        <App />
+      </SWRConfig>
     </BrowserRouter>
   </React.StrictMode>
 );
