239bb3b0160a5cd0b2ef4b9634b9d7c305e3dc88
Pranshu <pranshunayak566@gmail.com>
2022-07-19 17:18:15 +0530

    feat(api): add model for club registration requests

--- api/api/clubs/models.py
+++ api/api/clubs/models.py
@@ -1,4 +1,7 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+
+User = get_user_model()
 
 
 class Club (models.Model):
@@ -17,3 +20,26 @@ class Club (models.Model):
     description = models.TextField()
     email = models.EmailField(max_length=50, unique=True)
     logo = models.URLField(max_length=500)
+
+
+class ClubRegistrationRequests(models.Model):
+    STATUS_PENDING = 'Pending'
+    STATUS_APPROVED = 'Approved'
+    STATUS_REJECTED = 'Rejected'
+
+    STATUS_CHOICES = [
+        ('Pending', STATUS_PENDING),
+        ('Approved', STATUS_APPROVED),
+        ('Rejected', STATUS_REJECTED)
+    ]
+
+    user = models.ForeignKey(User, related_name='registered_clubs', on_delete=models.CASCADE)
+    club = models.ForeignKey(Club, related_name='registration_requests', on_delete=models.CASCADE)
+    fee_submitted = models.BooleanField(default=False)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default=STATUS_PENDING)
+    remark = models.TextField(blank=True)
+    updated_by = models.ForeignKey(
+        User, related_name='updated_club_registration_requests',
+        null=True, on_delete=models.SET_NULL)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
