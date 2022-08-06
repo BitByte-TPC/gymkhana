18aa2e5c29fca9cee5e204e40b9659993c00b8e9
Armin Patel <72291325+arminpatel@users.noreply.github.com>
2022-08-06 16:17:16 +0530

    feat(api): add endpoint to list and create events

---
+++ api/api/events/test_list_create_events.py
@@ -0,0 +1,173 @@
+import pytest
+from api.clubs.models import Club
+from api.events.models import Events
+from api.roles.models import Roles
+from django.contrib.auth import get_user_model
+from rest_framework import status
+from rest_framework.test import APIClient
+
+User = get_user_model()
+
+
+@pytest.mark.django_db
+@pytest.fixture(scope='function')
+def test_user(django_user_model):
+    return django_user_model.objects.create(email='test@user.com',
+                                            first_name='test',
+                                            last_name='User')
+
+
+@pytest.mark.django_db
+@pytest.fixture(scope='function')
+def test_club(django_user_model):
+    return Club.objects.create(name="Bitbyte - The Programming Club",
+                               category="S&T",
+                               description="Some desc",
+                               email="theprogclub@iiitdmj.ac.in",
+                               logo="https://www.iiitdmj.ac.in/webix.iiitdmj.ac.in/tpclogo.png")
+
+
+@pytest.fixture(scope='function')
+def client():
+    return APIClient()
+
+
+@pytest.mark.django_db
+def testListEvents_noEventsInDb_returnsEmptyList(test_user, client):
+    # given
+    client.force_authenticate(test_user)
+
+    # when
+    response = client.get('/events/', format='json')
+
+    # then
+    assert response.data['count'] == 0
+
+
+@pytest.mark.django_db
+def testListEvents_oneEventInDb_returnsOneEventInResponse(client, test_user, test_club):
+    # given
+    client.force_authenticate(test_user)
+
+    Events.objects.create(name='Introduction to Git and Github',
+                          description='some desc',
+                          starts_at='2022-08-07',
+                          ends_at='2022-08-07',
+                          image_url='https://asite.com',
+                          address='c',
+                          club=test_club)
+
+    # when
+    response = client.get('/events/', format='json')
+
+    # then
+    assert response.status_code == status.HTTP_200_OK
+    assert response.data['count'] == 1
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize('position', ['Core member', 'Coordinator', 'Co-Coordinator'])
+def testCreateEvents_coreMemberRequest_creationSuccessful(client, test_user, test_club, position):
+    # given
+    client.force_authenticate(test_user)
+    Roles.objects.create(name=position,
+                         club=test_club,
+                         user=test_user,
+                         assigned_at='2022-08-01')
+
+    # when
+    response = client.post('/events/', {'name': 'Introduction to Git and Github',
+                                        'description': 'some desc',
+                                        'starts_at': '2022-08-07',
+                                        'ends_at': '2022-08-07',
+                                        'image_url': 'https://asite.com',
+                                        'address': 'c',
+                                        'club': test_club.id
+                                        })
+
+    # then
+    created_event = Events.objects.all()[0]
+
+    assert response.status_code == status.HTTP_201_CREATED
+    assert response.data == {'id': created_event.id,
+                             'name': 'Introduction to Git and Github',
+                             'description': 'some desc',
+                             'starts_at': '2022-08-07',
+                             'ends_at': '2022-08-07',
+                             'registration_required': False,
+                             'created_by': None,
+                             'image_url': 'https://asite.com',
+                             'club': 1,
+                             'registration_approval_required': False,
+                             'address': 'c'}
+    assert created_event.name == 'Introduction to Git and Github'
+
+
+@pytest.mark.django_db
+def testCreateEvents_adminRequest_creationSuccessful(client, test_user, test_club):
+    # given
+    test_user.is_staff = True
+    client.force_authenticate(test_user)
+
+    # when
+    response = client.post('/events/', {'name': 'Introduction to Git and Github',
+                                        'description': 'some desc',
+                                        'starts_at': '2022-08-07',
+                                        'ends_at': '2022-08-07',
+                                        'image_url': 'https://asite.com',
+                                        'address': 'c',
+                                        'club': test_club.id
+                                        })
+
+    # then
+    created_event = Events.objects.all()[0]
+
+    assert response.status_code == status.HTTP_201_CREATED
+    assert response.data == {'id': created_event.id,
+                             'name': 'Introduction to Git and Github',
+                             'description': 'some desc',
+                             'starts_at': '2022-08-07',
+                             'ends_at': '2022-08-07',
+                             'registration_required': False,
+                             'created_by': None,
+                             'image_url': 'https://asite.com',
+                             'club': 1,
+                             'registration_approval_required': False,
+                             'address': 'c'}
+    assert created_event.name == 'Introduction to Git and Github'
+
+
+@pytest.mark.django_db
+def testCreateEvents_unauthorizedUserRequest_throwsForbidden(client, test_user, test_club):
+    # given
+    client.force_authenticate(test_user)
+
+    # when
+    response = client.post('/events/', {'name': 'Introduction to Git and Github',
+                                        'description': 'some desc',
+                                        'starts_at': '2022-08-07',
+                                        'ends_at': '2022-08-07',
+                                        'image_url': 'https://asite.com',
+                                        'address': 'c',
+                                        'club': test_club.id
+                                        })
+    # then
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+@pytest.mark.django_db
+def testCreateEvents_missingDataInRequest_throwsBasRequest(client, test_user, test_club):
+    # given
+    client.force_authenticate(test_user)
+
+    # when
+    # request missing start and end time
+    response = client.post('/events/', {'name': 'Introduction to Git and Github',
+                                        'description': 'some desc',
+                                        'image_url': 'https://asite.com',
+                                        'address': 'c',
+                                        'club': test_club.id
+                                        })
+
+    # then
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
---
+++ api/api/events/views.py
@@ -0,0 +1,27 @@
+from django.db.models import Q
+from rest_framework.exceptions import PermissionDenied
+from rest_framework.generics import ListCreateAPIView
+
+from .models import Events
+from .serializers import EventSerializer
+
+
+class ListCreateEventsView(ListCreateAPIView):
+    """ View to list and create users """
+    queryset = Events.objects.all()
+    serializer_class = EventSerializer
+
+    def perform_create(self, serializer):
+        if self._has_create_permission(serializer.validated_data):
+            serializer.save()
+        else:
+            raise PermissionDenied({"message": "You are not allowed to perform this action"})
+
+    # Allow only Core members (including Coordinator and Co-Coordinator)
+    # and admins to create events
+    def _has_create_permission(self, validated_data):
+        allowed_roles = validated_data['club'].roles.filter(
+                        Q(name='Core member') | Q(name='Coordinator') | Q(name='Co-Coordinator'))
+        user_roles = self.request.user.roles.filter(club__id=validated_data['club'].id)
+
+        return (self.request.user.is_staff or (allowed_roles & user_roles).exists())
