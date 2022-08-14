35af3091614f1ef63c06790556ca1ef6cef4d4b8
Armin Patel <72291325+arminpatel@users.noreply.github.com>
2022-08-14 23:38:07 +0530

    fix(api): add makemigration for api_events to makemigrations.sh

--- api/scripts/makemigrations.sh
+++ api/scripts/makemigrations.sh
@@ -2,4 +2,5 @@
 python3 manage.py makemigrations api_accounts &&
 python3 manage.py makemigrations api_auth && 
 python3 manage.py makemigrations api_clubs &&
+python3 manage.py makemigrations api_events &&
 python3 manage.py makemigrations
