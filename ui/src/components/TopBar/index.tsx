d54ee8cc79d82587b27cd6f539c7072d97a986fc
Varun Singh <varun7singh10@gmail.com>
2022-09-01 10:52:40 +0530

    feat(ui): on clicking profile icon redirect to profile

--- ui/src/components/TopBar/index.tsx
+++ ui/src/components/TopBar/index.tsx
@@ -1,11 +1,13 @@
 import styles from './styles.module.scss';
 import {logo} from '../../assets';
 import {ThemeButton} from '../Theme';
+import {useNavigate} from 'react-router-dom';
 interface TopBarProps {
   setSidebarVisibility: (isSidebarOpenOnMobile: boolean) => void;
 }
 
 export const TopBar: React.FC<TopBarProps> = ({setSidebarVisibility}) => {
+  const navigate = useNavigate();
   const menuClickHandler = () => {
     setSidebarVisibility(true);
   };
@@ -16,7 +18,12 @@ export const TopBar: React.FC<TopBarProps> = ({setSidebarVisibility}) => {
       <div className={styles.logoContainer} onClick={menuClickHandler}>
         <img className={styles.logo} src={logo} alt="logo" />
       </div>
-      <div className={styles.profileInfoContainer}>
+      <div
+        className={styles.profileInfoContainer}
+        onClick={() => {
+          navigate('/profile');
+        }}
+      >
         <img
           src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png"
           alt="profile-img"
